069b55e8769a098a58121bfb792641821f3ff347
mghaneh1375 <mghaneh1375@yahoo.com>
2020-07-13 22:56:30 +0430

    second commit

--- resources/views/layouts/siteStructure3.blade.php
+++ resources/views/layouts/siteStructure3.blade.php
@@ -27,6 +27,324 @@
 
         <link rel="stylesheet" href="{{\Illuminate\Support\Facades\URL::asset("css/chatbox.css")}}">
 
+        <style>
+
+            * {
+                outline: none!important;
+            }
+
+            a {
+                color: #4a5f73;
+                text-decoration: none;
+            }
+            a {
+                background-color: transparent;
+                -webkit-text-decoration-skip: objects;
+            }
+
+            .c-swiper-specials__title img {
+                width: 100%;
+                max-height: calc(100% - 41px);
+                -o-object-fit: contain;
+                object-fit: contain;
+                -webkit-box-flex: 1;
+                -ms-flex: 1;
+                flex: 1;
+                border-style: none;
+                line-height: 22px;
+            }
+
+            .firstWeek {
+                background: #ef394e;
+                height: 780px;
+            }
+
+            .secondWeek {
+                background: #6bb927;
+            }
+
+            .c-swiper-specials {
+                width: 100%;
+                margin-top: 16px;
+            }
+
+            .c-swiper-specials .container {
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: flex;
+                -webkit-box-align: stretch;
+                -ms-flex-align: stretch;
+                align-items: stretch;
+                -webkit-box-pack: start;
+                -ms-flex-pack: start;
+                justify-content: flex-start;
+                padding: 35px 16px;
+                background-size: auto 100%;
+                max-height: 500px;
+            }
+
+            .c-swiper-specials__title {
+                margin: 0 44px;
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: flex;
+                -webkit-box-align: start;
+                -ms-flex-align: start;
+                align-items: flex-start;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                justify-content: center;
+                -webkit-box-orient: vertical;
+                -webkit-box-direction: normal;
+                -ms-flex-direction: column;
+                flex-direction: column;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+                width: 170px;
+            }
+
+            .c-swiper-specials__btn {
+                padding: 9px 16px;
+                font-size: 1rem;
+                cursor: pointer;
+                line-height: 1.571;
+                font-weight: 700;
+                border: 1px solid #fff;
+                border-radius: 5px;
+                color: #fff;
+                text-decoration: none;
+                box-shadow: none;
+                user-select: none;
+                display: inline-flex;
+                -webkit-box-align: center;
+                align-items: center;
+                -webkit-box-pack: center;
+                justify-content: center;
+                background: none;
+                transition: all .3s ease-in-out;
+                position: relative;
+            }
+
+            .c-swiper--specials, .c-swiper--specials .c-box {
+                background: transparent;
+            }
+            .c-swiper--specials {
+                margin-top: 0!important;
+                -webkit-box-flex: 0;
+                -ms-flex: 0 0 calc(100% - 258px);
+                flex: 0 0 calc(100% - 258px);
+                width: calc(100% - 258px);
+                max-width: calc(100% - 258px);
+                -webkit-box-shadow: none;
+                box-shadow: none;
+                padding: 0;
+            }
+
+            .c-swiper--products {
+                margin: 16px 0 0;
+                padding: 9px 33px 11px 0;
+                max-width: 100%;
+            }
+            .c-box:first-child {
+                margin-top: 0;
+            }
+
+            .c-box {
+                background-color: #fff;
+                position: relative;
+            }
+
+            .c-box .swiper-container {
+                position: unset;
+                padding-top: 3px;
+            }
+
+            .swiper-container {
+                margin: 0 auto;
+                position: relative;
+                overflow: hidden;
+                list-style: none;
+                padding: 0;
+                z-index: 1;
+            }
+
+            .c-swiper--products .swiper-wrapper {
+                padding: 2px 0;
+            }
+
+            .swiper-slide {
+                -ms-flex-negative: 0;
+                flex-shrink: 0;
+                height: 100%;
+                width: 240px;
+                display: block;
+                float: right;
+                background: no-repeat 50% 50%;
+                background-size: cover;
+                position: relative;
+                -webkit-transition-property: -webkit-transform;
+                transition-property: -webkit-transform;
+                transition-property: transform;
+                transition-property: transform,-webkit-transform;
+            }
+
+            .c-product-box__box-link {
+                width: 100%;
+                height: 100%;
+                position: absolute;
+                background: transparent;
+                z-index: 2;
+            }
+            .c-swiper-specials--incredible .c-product-box {
+                min-height: 385px;
+            }
+            .c-swiper--specials .c-product-box {
+                padding-bottom: 25px;
+            }
+            .c-swiper--products .c-product-box {
+                -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+                border: none;
+                border-radius: 5px;
+                background-color: #fff;
+                text-align: center;
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: flex;
+                -webkit-box-orient: vertical;
+                -webkit-box-direction: normal;
+                -ms-flex-direction: column;
+                flex-direction: column;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                align-items: center;
+                margin: 0 5px;
+                -webkit-box-pack: start;
+                -ms-flex-pack: start;
+                justify-content: flex-start;
+                padding: 5px 15px 16px;
+                position: relative;
+            }
+
+            .c-swiper--products .c-product-box__img {
+                width: 180px;
+                height: 180px;
+                text-align: center;
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: flex;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                align-items: center;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                justify-content: center;
+                -ms-flex-negative: 0;
+                flex-shrink: 0;
+                max-width: 100%;
+                position: relative;
+            }
+
+            .c-product-box__title {
+                display: block;
+                overflow: hidden;
+                font-size: 12px;
+                font-size: .857rem;
+                line-height: 30px;
+                text-align: right;
+                line-height: 22px;
+                margin-top: 13px;
+                height: 43px;
+            }
+            .c-product-box--card-macro .c-product-box__row--price {
+                position: relative;
+                top: 10px;
+                margin-bottom: 10px;
+            }
+            .c-product-box__row--price {
+                margin-top: 13px;
+                height: 43px;
+            }
+            .c-product-box__row {
+                display: -webkit-inline-box;
+                display: -ms-inline-flexbox;
+                display: inline-flex;
+                -webkit-box-pack: justify;
+                -ms-flex-pack: justify;
+                justify-content: space-between;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                align-items: center;
+                width: 100%;
+                -webkit-box-orient: horizontal;
+                -webkit-box-direction: reverse;
+                -ms-flex-direction: row-reverse;
+                flex-direction: row-reverse;
+            }
+
+            .swiper-container {
+                list-style: none;
+            }
+
+            .c-product-box__img img {
+                max-width: 100%;
+                max-height: 100%;
+                -o-object-fit: contain;
+                object-fit: contain;
+            }
+
+            #chat-bot-launcher-container.chat-bot-launcher-enabled #chat-bot-launcher {
+                visibility: visible !important;
+                animation: launcher-frame-appear 0.25s ease forwards;
+            }
+
+            #chat-bot-launcher-container {
+                position: fixed;
+                bottom: 20px;
+                right: 20px;
+                direction: ltr !important;
+                z-index: 2147482999;
+            }
+
+            #chat-bot-launcher-container .chat-bot-launcher {
+                font-family: "Open Sans",sans-serif;
+                height: 42px;
+                visibility: hidden;
+                -webkit-transform: translateZ(0);
+                border-radius: 5em;
+                cursor: pointer;
+                padding: 0px 20px;
+                -webkit-transition: -webkit-transform .15s ease-in-out, box-shadow .15s ease-in-out!important;
+                -moz-transition: -moz-transform .15s ease-in-out, box-shadow .15s ease-in-out!important;
+                -o-transition: -o-transform .15s ease-in-out, box-shadow .15s ease-in-out!important;
+                transition: transform .15s ease-in-out, box-shadow .15s ease-in-out!important;
+            }
+
+            .chat-bot-flex-center {
+                justify-content: center;
+            }
+
+            .chat-bot-flex-center, .chat-bot-flex-end {
+                display: -webkit-box;
+                display: -webkit-flex;
+                display: -moz-box;
+                display: -ms-flexbox;
+                display: flex;
+                align-items: center;
+            }
+
+            .container {
+                width: 100% !important;
+            }
+
+            .selectedTag {
+                background-color: yellow;
+            }
+
+        </style>
+
 
         <style>
             .profileIconBox {
@@ -110,6 +428,67 @@
         </style>
 
 
+        <style>
+            .banner {
+                height: 200px;
+                display: flex;
+                justify-content: center;
+                position: relative;
+            }
+            .bannerGrayBox {
+                width: 83%;
+                height: 200px;
+                background-color: #f9f9f9;
+                position: absolute;
+                z-index: 1;
+            }
+            .bannerBorderBox {
+                width: 78%;
+                height: 210px;
+                position: absolute;
+                top: 30px;
+                z-index: 10;
+            }
+            .bannerLightRedBox {
+                background-color: lightcoral;
+            }
+            .bannerLightBlueBox {
+                background-color: lightblue;
+            }
+            .bannerLightGreenBox {
+                background-color: lightgreen;
+            }
+            .bannerMainBox {
+                width: 76%;
+                height: 220px;
+                position: absolute;
+                top: 40px;
+                z-index: 20;
+                background-size: 64%;
+                background-repeat: no-repeat;
+            }
+            .projectsBanner {
+                background-image: url("{{URL::asset('images/projectsBanner.png')}}");
+                background-color: #f15b47;
+            }
+            .servicesBanner {
+                background-image: url("{{URL::asset('images/servicesBanner.png')}}");
+                background-color: rgb(155, 193, 60);
+            }
+            .productsBanner {
+                background-image: url("{{URL::asset('images/productsBanner.png')}}");
+                background-color: rgb(51, 163, 220);
+            }
+            .bannerText {
+                font-size: 3em;
+                font-weight: 700;
+                color: white;
+                position: absolute;
+                bottom: 20%;
+                right: 10%;
+            }
+        </style>
+
     @show
 </head>
 
@@ -152,6 +531,19 @@
 {{--            <div onclick="document.location.href = '{{route('profile')}}'" class="myNavbar">پروفایل</div>--}}
         </div>
     </div>
+
+    @yield("banner")
+
+
+    @if(isset($tags))
+        <div style="margin: 100px 12% 25px; text-align: center;">
+            <button id="allTags" class="tagFilter selectedTag" data-status="1" data-filter="-1">همه موارد</button>
+            @foreach($tags as $tag)
+                <button data-status="0" class="tagFilter" data-filter="{{$tag->id}}">{{$tag->name}}</button>
+            @endforeach
+        </div>
+    @endif
+
 
     @yield("content")
 
@@ -349,6 +741,52 @@
                 }
             });
         }
+
+    </script>
+
+    <script>
+
+        $(document).ready(function () {
+
+            $(".tagFilter").on('click', function () {
+
+                var status = $(this).attr("data-status");
+                $(".tagFilter").attr("data-status", "0").removeClass("selectedTag");
+
+                var selectedTag;
+
+                if(status == "0") {
+                    $(this).attr("data-status", "1").addClass("selectedTag");
+                    selectedTag = $(this).attr("data-filter");
+                }
+                else {
+                    $(this).attr("data-status", "0").removeClass("selectedTag");
+                    selectedTag = -2;
+                }
+
+                if(selectedTag == "-1") {
+
+                    if(status == "0") {
+                        $(".myItem").removeClass("hidden");
+                    }
+                    else {
+                        $(".myItem").addClass("hidden");
+                    }
+                    return;
+                }
+
+                $(".myItem").addClass("hidden").each(function () {
+
+                    if($(this).attr("data-tag").includes("-" + selectedTag + "-")) {
+                        $(this).removeClass("hidden");
+                    }
+
+                });
+
+            });
+
+
+        });
 
     </script>
 
